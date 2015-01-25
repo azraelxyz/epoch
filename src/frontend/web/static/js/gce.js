478a32fbfe1de0dda55fdd75573ce065e6cf7c99
xwhuang <xwhuang@gmail.com>
2015-01-26 00:13:38 +0800

    remove unused code

--- src/frontend/web/static/js/gce.js
+++ src/frontend/web/static/js/gce.js
@@ -15,21 +15,10 @@ google.appengine.epoch.getEpoch = function(sec, scope, cbk) {
     );
 };
 
-google.appengine.epoch.register = function() {
-  var convertBtn = document.querySelector('#convert');
-  convertBtn.addEventListener('click', function(e) {
-    google.appengine.epoch.getEpoch(document.querySelector('#epoch').value);
-  });
-};
-
 google.appengine.epoch.init = function(apiRoot) {
   var apisToLoad;
   var callback = function() {
-    if (--apisToLoad == 0) {
-      //google.appengine.epoch.register();
-    }
   }
 
-  apisToLoad = 1; // must match number of calls to gapi.client.load()
   gapi.client.load('epoch', 'v1', callback, apiRoot);
 };
